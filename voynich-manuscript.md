43d5e59b41803ae2842af5de64b0c4dcc63ee0ed
Andrei Regiani <andrei.cpp@gmail.com>
2016-09-20 15:38:44 +0300

    Added new section to website: Voynich Manuscript

---
+++ voynich-manuscript.md
@@ -0,0 +1,14 @@
+---
+layout: page
+title: Voynich Manuscript
+permalink: /voynich-manuscript/
+---
+
+This is my research and analysis using modern techniques of data analysis to discover useful information about this mysterious manuscript. My goal is to create tools and generate reliable information that can be tested used by others.
+
+I will also gather and curate previous research done by other people, keep in mind there are many pseudo-scientific blogs and YouTube videos that claimed to have deciphered it, with unconvincing, sometimes insane hypothesis.
+
+## Background
+Voynich Manuscript is an unique illustrated codex hand-written in early 15th century in an unknown language and writing system. More background information can be found on [Wikipedia - Voynich Manuscript](https://en.wikipedia.org/wiki/Voynich_manuscript){:target="_blank"}
+
+## Transcription
---
+++ voynich/viewer/viewer.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $("#buttonViewScan").click(function() {
+        var url = "https://www.jasondavies.com/voynich/#" + $('#folio-select option:selected').text();
+        window.open(url, '_blank');
+    });
+
+    $("#buttonVoynich").click(function() {
+        $("#voynich-text").css("font-family", "Voynich");
+        $("#buttonVoynich").attr('class', 'btn btn-primary');
+        $("#buttonTranscription").attr('class', 'btn btn-default');
+    });
+
+    $("#buttonTranscription").click(function() {
+        $("#voynich-text").css("font-family", "Lucida Console, Monaco, monospace");
+        $("#buttonVoynich").attr('class', 'btn btn-default');
+        $("#buttonTranscription").attr('class', 'btn btn-primary');
+    });
+
+    $(".word").mouseenter(function() {
+        $(this).toggleClass('transcription-font');
+    }).mouseleave(function() {
+        $(this).toggleClass('transcription-font');
+    });
+
+    $('#folio-select').on('change', function() {
+        $('#voynich-text').html('<p style="font-family: Lucida Console">Loading...</p>');
+        var folio = "http://127.0.0.1:4000/voynich/folios/" + this.value + ".html";
+        $.ajax({
+            url: folio,
+            type: 'GET',
+            dataType: 'html',
+            success: function(data) {
+                $('#voynich-text').html(data);
+            }
+        });
+    });
+});
